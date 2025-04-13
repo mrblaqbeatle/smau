0a937201381babaf66d44695bf2f019ef4793756
mrblaqbeatle <ezraalpha787@gmail.com>
2025-04-13 13:45:54 +0300

    V1.1: We have had multiple versions but this is the first live one

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcxwqjoFEUYKTwiuQKMkAfG9y9HZFPCVg",
+  authDomain: "smau-256.firebaseapp.com",
+  projectId: "smau-256",
+  storageBucket: "smau-256.firebasestorage.app",
+  messagingSenderId: "76824679086",
+  appId: "1:76824679086:web:938d91f86a44c67ec3533f",
+  measurementId: "G-E18K7HRKBQ",
+  databaseURL: "https://smau-256-default-rtdb.firebaseio.com/"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+function formatUGX(num) {
+  return "UGX " + num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function updateUI(balances) {
+  document.getElementById('fixed-account').textContent = formatUGX(balances.fixed);
+  document.getElementById('savings').textContent = "Savings: " + formatUGX(balances.savings);
+  document.getElementById('investment').textContent = formatUGX(balances.investment);
+  document.getElementById('total-funds').textContent = formatUGX(balances.fixed + balances.savings + balances.investment);
+}
+
+function getBalances() {
+  db.ref("balances").once("value", snapshot => {
+    if (snapshot.exists()) {
+      updateUI(snapshot.val());
+    }
+  });
+}
+
+function updateBalances() {
+  db.ref("balances").once("value", snapshot => {
+    if (snapshot.exists()) {
+      let data = snapshot.val();
+      data.savings += 230;
+      const decrease = Math.floor(Math.random() * (420 - 240 + 1)) + 240;
+      data.savings = Math.max(0, data.savings - decrease);
+      db.ref("balances").set(data);
+      updateUI(data);
+    }
+  });
+}
+
+getBalances();
+setInterval(updateBalances, 1800000); // every 30 minutes
